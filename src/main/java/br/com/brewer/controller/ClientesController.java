72eef4ee6ea0bb9c34a13c76ce82402b1871f414
Andre Girao <andrelgirao22@hotmail.com>
2019-01-02 13:25:28 -0200

    prototipado a tela de cadastro de usuarios

--- src/main/java/br/com/brewer/controller/ClientesController.java
+++ src/main/java/br/com/brewer/controller/ClientesController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ClienteController {
+public class ClientesController {
 
 	@RequestMapping(value="/clientes/novo")
 	public String novo() {
