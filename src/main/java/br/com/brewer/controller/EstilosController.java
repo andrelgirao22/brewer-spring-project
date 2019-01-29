72d2055398d6a620ae7fa06a2b8480b026bcae23
Andre Girao <andrelgirao22@hotmail.com>
2019-01-29 13:50:13 -0200

    criado o cadastro de estilos

--- src/main/java/br/com/brewer/controller/EstilosController.java
+++ src/main/java/br/com/brewer/controller/EstilosController.java
@@ -1,14 +1,51 @@
 package br.com.brewer.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.com.brewer.model.Cerveja;
+import br.com.brewer.model.Estilo;
+import br.com.brewer.repository.Estilos;
+import br.com.brewer.service.CadastroCervejaService;
+import br.com.brewer.service.CadastroEstiloService;
+import br.com.brewer.service.exception.NomeEstiloJaCadastradoException;
 
 @Controller
 public class EstilosController {
 
+	
+	@Autowired
+	private CadastroEstiloService cadastroEstiloService;
+	
 	@RequestMapping("estilos/novo")
-	public String novo() {
-		return "estilo/CadastroEstilo";
+	public ModelAndView novo(Estilo estilo) {
+		ModelAndView mv = new ModelAndView("estilo/CadastroEstilo");
+		return mv;
+	}
+	
+	@RequestMapping(value="/estilos/novo", method=RequestMethod.POST)
+	public ModelAndView cadastrar(@Valid Estilo estilo, BindingResult result, Model model, RedirectAttributes attributes) {
+		
+		if(result.hasErrors()) {
+			return this.novo(estilo);
+		}
+		
+		try {
+			cadastroEstiloService.salvar(estilo);
+		} catch(NomeEstiloJaCadastradoException ex) {
+			result.rejectValue("nome", ex.getMessage(), ex.getMessage());
+			return this.novo(estilo);
+		}
+		attributes.addFlashAttribute("mensagem", "Estilo salvo com sucesso");
+		return new ModelAndView("redirect:/estilos/novo");
 	}
 	
 }
---
+++ src/main/java/br/com/brewer/service/CadastroEstiloService.java
@@ -0,0 +1,30 @@
+package br.com.brewer.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.brewer.model.Estilo;
+import br.com.brewer.repository.Estilos;
+import br.com.brewer.service.exception.NomeEstiloJaCadastradoException;
+
+@Service
+public class CadastroEstiloService {
+
+	@Autowired
+	private Estilos estilos;
+	
+	@Transactional
+	public void salvar(Estilo estilo) {
+		
+		Optional<Estilo> esOptional = estilos.findByNomeIgnoreCase(estilo.getNome());
+		
+		if(esOptional.isPresent()) {
+			throw new NomeEstiloJaCadastradoException("Nome do estilo já cadastrado");
+		}
+		estilos.save(estilo);
+	}
+	
+}
